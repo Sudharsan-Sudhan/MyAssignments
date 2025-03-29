bf50d27f0675b9527e95ff6c37631fbd06087387
Sudharsan-Sudhan <ssudharsancs91.myl@gmail.com>
2025-03-29 22:29:01 +0530

    assignments of day 1 from week 1 is uploaded

---
+++ week1/day1/Chrome.java
@@ -0,0 +1,34 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		
+//		 TODO Auto-generated method stub
+
+//		version of type float with a value of 91.0f
+		float version = 91.0f;
+		
+//		developer of type String with a value of "Google"
+		String developer = "Google";
+		
+//		isBeta of type boolean with a value of false
+		boolean isBeta = false;
+		
+//		releaseYear of type int with a value of 2008
+		int releaseYear = 2008;
+		
+//		shortcutKey of type char with a value of 'C'
+		char shortcutKey = 'C'; 
+		
+		System.out.println("Variable named version is declared as float with the value : " + version);
+		
+		System.out.println("Variable named developer is declared as String with the value : " + developer);
+		
+		System.out.println("Variable named isBeta is declared as Boolean with the value : " + isBeta);
+		
+		System.out.println("Variable named releaseYear is declared as integer with the value : " + releaseYear);
+		
+		System.out.println("Variable named shortcutKey is declared as char with the value : " + shortcutKey);
+	}
+}
