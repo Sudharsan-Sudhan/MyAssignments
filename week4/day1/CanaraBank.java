886639853e1d1c06e044863984bd5e0a6cee4fca
Sudharsan-Sudhan <ssudharsancs91.myl@gmail.com>
2025-05-10 08:12:07 +0530

    Delete week4 directory

--- week4/day1/CanaraBank.java
+++
@@ -1,8 +0,0 @@
-package week4.day1;
-
-public abstract class CanaraBank implements Payments {
-	public void recordPaymentDetails() {
-		System.out.println("record Payment Details - Canara Bank");
-	}
-
-}
--- week4/day1/MySqlConnection.java
+++
@@ -1,9 +0,0 @@
-package week4.day1;
-
-public abstract class MySqlConnection implements DatabaseConnection {
-	
-	public void executeQuery() {
-		System.out.println("Execute Query from My SQL Connection");
-	}
-
-}
