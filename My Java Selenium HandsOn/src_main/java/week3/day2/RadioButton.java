70ffd39a3439a36231762c11de294e2f0310f22c
Sudharsan-Sudhan <ssudharsancs91.myl@gmail.com>
2025-05-14 19:03:14 +0530

    Delete My Java Selenium HandsOn/src_main/java directory

--- My Java Selenium HandsOn/src_main/java/week3/day2/RadioButton.java
+++
@@ -1,8 +0,0 @@
-package week3.day2;
-
-public class RadioButton extends Button{
-	public void selectRadioButton() {
-		System.out.println("User is able to select the radio button");
-	}
-
-}
--- My Java Selenium HandsOn/src_main/java/week3/day2/TextField.java
+++
@@ -1,9 +0,0 @@
-package week3.day2;
-
-public class TextField extends WebElement {
-	
-	public void getText() {
-		System.out.println("User is able to get the text");
-	}
-
-}
